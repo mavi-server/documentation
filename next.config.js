fddf2180e66e0cc4ce83812ad36c98803baa57f9
m.emre yalçın <99.emreyalcin@gmail.com>
2022-08-07 00:49:37 +0300

    fix: eslint issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withNextra = require('nextra')({
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
--- theme.config.js
+++ theme.config.js
@@ -1,5 +1,24 @@
-import Head from './components/Head'
-import MaviLogo from './components/Logo'
+import Image from 'next/image'
+
+const MaviLogo = () => {
+  return (
+    <Image
+      width="120px"
+      src="https://raw.githubusercontent.com/m-emre-yalcin/mavi/main/public/mavi.svg"
+    />
+  )
+}
+const Head = () => {
+  return (
+    <>
+      <title>Mavi documantation</title>
+      <link rel="shortcut icon" href="../public/mavi.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Mavi documantation" />
+      <meta name="og:title" content="Mavi documantation" />
+    </>
+  )
+}
 
 export default {
   projectLink: 'https://github.com/mavi-server/mavi', // GitHub link in the navbar
